59604ab3df531e026efc3ab7ba2003cb9a0e0fa0
José David Medina Paredes <cosoacount@gmail.com>
2024-06-21 06:38:57 -0600

    Admin idea

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -10,9 +10,18 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Tiny5&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/app.css">
 </head>
-<body>
-    <?php 
-        echo $contenido;
-    ?>
+<body class="lg:h-screen flex flex-col lg:flex-row">
+    <aside class="bg-zinc-700 lg:h-full lg:w-1/4 p-2">
+        <a href="/" class="flex justify-center items-center md:gap-2 py-3">
+            <img src="/img/LogoPixelMarket.png" alt="Logo Pixel Market" class="max-w-12">
+            <h1 class="font-display text-3xl lg:text-4xl xl:text-5xl text-zinc-200 text-center">Pixel Market</h1>
+        </a>
+    </aside>
+    <main class="flex flex-col items-center lg:h-full lg:overflow-y-auto lg:w-3/4">
+        <h2 class="font-display text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-zinc-700 text-center py-10"><?php echo $titulo; ?></h2>
+        <?php 
+            echo $contenido;
+        ?>
+    </main>
 </body>
 </html>
